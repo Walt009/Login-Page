87c8794c392d0f1859f48bda100561dd0f8c9f11
Walt009 <walt40028922@gmail.com>
2023-03-10 21:28:28 -0300

    Primeiro commit, Login feito !

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+// Criando variáveis:
+let email = document.getElementById("email");
+let senha = document.getElementById("senha");
+
+function retornoDoLogin(){
+    let emailDigitado = email.value;
+    let senhaDigitada = senha.value;
+
+    console.log(emailDigitado);
+    console.log(senhaDigitada);
+
+}
